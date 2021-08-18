3550ae4b7e429d7ae48425b05f72a227657b1ee9
callum day ham <caldayham@gmail.com>
2021-08-18 01:43:02 -0500

    Initial Commit

---
+++ Waking Note/Waking_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Waking_NoteApp.swift
+//  Waking Note
+//
+//  Created by Callum Day Ham on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Waking_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
